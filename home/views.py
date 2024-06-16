0b26c079600825ae6b4b2941693ca208a71a08ea
vedh18 <vedhanthbala007@gmail.com>
2024-06-16 20:58:39 +0530

    Template, interconnection, variables, dynammic link usage

--- home/views.py
+++ home/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home')
+rooms = [
+    {'id' : 1, 'name' : 'SpeakJS'}, 
+    {'id' : 2, 'name' : 'The Coding Den'},
+    {'id' : 3, 'name' : 'Programmer Humor'},
+]
 
-def room(request):
-    return HttpResponse('ROOM')
+def home(request):
+    variables = {'rooms' : rooms}
+    return render(request, 'home.html', variables)
+
+def room(request, pk):
+    return render(request, 'room.html')

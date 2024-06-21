df7e0d172e32589d4408089a6c2c8cf74eb5fa69
vedh18 <vedhanthbala007@gmail.com>
2024-06-21 23:33:34 +0530

    Creating models, making migrations, one to many relationships in models using foreign key, to register every model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True) 
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null = True, blank=True)
+    #participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # When we print the class, it prints the name.
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    # The below two lines depict how every message is related to both the user which sent it and the room it is a part of.'
+    # Precisly how user can write many messages and a room can have many messages but a message can have only one room and one user.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
--- home/views.py
+++ home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id' : 1, 'name' : 'SpeakJS'}, 
-    {'id' : 2, 'name' : 'The Coding Den'},
-    {'id' : 3, 'name' : 'Programmer Humor'},
-]
+# rooms = [
+#     {'id' : 1, 'name' : 'SpeakJS'}, 
+#     {'id' : 2, 'name' : 'The Coding Den'},
+#     {'id' : 3, 'name' : 'Programmer Humor'},
+# ]
 
 def home(request):
-    variables = {'rooms' : rooms}
+    rooms = Room.objects.all()
+    variables = {'rooms': rooms}
     return render(request, 'home.html', variables)
 
 def room(request, pk):
-    return render(request, 'room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
+    return render(request, 'room.html', context)
